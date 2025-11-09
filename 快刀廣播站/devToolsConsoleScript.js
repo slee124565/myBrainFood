4b073fb09fb747423994d154074631636f522d42
Lee Shiueh <lee.shiueh@gmail.com>
2025-11-09 20:52:45 +0800

    add 劉瀾學習力

---
+++ 快刀廣播站/devToolsConsoleScript.js
@@ -0,0 +1,8 @@
+
+設計 Chrom Browser DevTools Console 簡單腳本方式 存取網頁 HTML 內容中，以下幾個 div class 鎖定的目標 element 的 outerHTML 內容，然後組成同一個文字訊息，透過 browser 檔案下載的方式在本地端儲存，檔案名稱為 <div class="article-title iget-common-c1"> element 的 innerText 所構成的 "{article-title.innerText}.html"
+
+1. article-cover-wrap
+2. article-title iget-common-c1
+3. article-info-className
+4. article-body
+5. article-time-info iget-common-c3 iget-common-f4
